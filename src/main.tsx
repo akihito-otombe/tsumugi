085ddd4a3239e90de926e1d7014f1e663edc111b
akihito-otombe <akihito.otombe@bee-arc.com>
2025-11-05 19:30:48 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import TsumugiApp from './App'
+import { createRoot } from 'react-dom/client'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <TsumugiApp />
+    <App />
   </React.StrictMode>
 )
