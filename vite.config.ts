90c61ef75f293354476a3475cff8d496c5ba4b25
akihito-otombe <akihito.otombe@bee-arc.com>
2025-11-05 14:53:39 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  // base は書かない（Workflow の --base で毎回上書きする）
 });
