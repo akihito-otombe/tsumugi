49c694eb41c03247d75df54a272d811a80b351f5
akihito-otombe <akihito.otombe@bee-arc.com>
2025-11-05 16:05:36 +0900

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  // base は書かない（Workflow の --base で毎回上書きする）
-});
